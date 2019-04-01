e35d00c5f1be03a506cb24f5163fc532c7c731dc
Henrik Slaaen <henrik@slaaen.com>
2019-04-01 19:49:51 +0200

    Added headerfile for all structs to be used in the project

---
+++ core/types/networktypes.h
@@ -0,0 +1,18 @@
+#ifndef networktypes
+#define networktypes
+
+typedef struct Graph {
+    // Graph
+} Graph;
+
+typedef struct Node {
+    // Node
+} Node;
+
+typedef struct Connection {
+    int weight;
+    struct Node destination;
+} Connection;
+
+
+#endif
