7678206c80cbab007c4fb9d0a6056fd23cead6d9
Henrik Slaaen <henrik@slaaen.com>
2019-04-02 16:43:07 +0200

    Changed the creation of testnodes, and testing the print_node function

--- core/routing_server.c
+++ core/routing_server.c
@@ -9,6 +9,7 @@
 #include <math.h>
 
 #include "../util/logger.h"
+#include "../util/print.h"
 #include "types/networktypes.h"
 
 struct Node* create_test_nodes(int numberOfNodes);
@@ -19,6 +20,16 @@ int main() {
     
     struct Node *testnodes = malloc(sizeof(struct Node) * 8);;
     testnodes = create_test_nodes(8);
+    
+    print_node(testnodes[0]);
+    print_node(testnodes[1]); 
+    print_node(testnodes[2]);
+    print_node(testnodes[3]);
+    print_node(testnodes[4]);
+    print_node(testnodes[5]);
+    print_node(testnodes[6]);
+    print_node(testnodes[7]);
+    
     return 0;
 }
 
@@ -28,159 +39,136 @@ struct Node* create_test_nodes(int numberOfNodes) {
    
     
     // New Node    
-    n->own_address = 1;
-    n->number_of_connections = 2;
-    n->connections = malloc(sizeof(struct Connection) * 2);
-    n->connections->destination = 11;
-    n->connections->weight = 2;
+    n[0].own_address = 1;
+    n[0].number_of_connections = 2;
+    n[0].connections = malloc(sizeof(struct Connection) * 2);
     
-    n->connections += 1;
-    n->connections->destination = 103;
-    n->connections->weight = 6;
+    n[0].connections[0].destination = 11;
+    n[0].connections[0].weight = 2;
+    
 
-    n += 1;
+    n[0].connections[1].destination = 103;
+    n[0].connections[1].weight = 6;
+    
 
     // New Node
-    n->own_address = 11;
+    n[1].own_address = 11;
 
-    n->number_of_connections = 3;
-    n->connections = malloc(sizeof(struct Connection) * 3); 
+    n[1].number_of_connections = 3;
+    n[1].connections = malloc(sizeof(struct Connection) * 3); 
     
-    n->connections->destination = 1;
-    n->connections->weight = 2;
+    n[1].connections[0].destination = 1;
+    n[1].connections[0].weight = 2;
     
-    n->connections += 1;
-
-
-    n->connections->destination = 19;
-    n->connections->weight = 2;
+    n[1].connections[1].destination = 19;
+    n[1].connections[1].weight = 2;
     
-    n->connections += 1;
+    n[1].connections[2].destination = 13;
+    n[1].connections[2].weight = 7;
 
-    n->connections->destination = 13;
-    n->connections->weight = 7;
 
-    n += 1;
     
     // New Node
-    n->own_address = 19;
+    n[2].own_address = 19;
     
-    
-    n->number_of_connections = 3;
-    n->connections = malloc(sizeof(struct Connection) * 3);
+    n[2].number_of_connections = 3;
+    n[2].connections = malloc(sizeof(struct Connection) * 3);
 
-    n->connections->destination = 11;
-    n->connections->weight = 2;
-    n->connections += 1;
+    n[2].connections[0].destination = 11;
+    n[2].connections[0].weight = 2;
 
+    n[2].connections[1].destination = 101;
+    n[2].connections[1].weight = 2;
 
-    n->connections->destination = 101;
-    n->connections->weight = 2;
-    n->connections += 1;
-
-
-    n->connections->destination = 103;
-    n->connections->weight = 1;
-    n->connections += 1;
+    n[2].connections[2].destination = 103;
+    n[2].connections[2].weight = 1;
 
 
-    n += 1;
 
     // New Node
-    n->own_address = 103;
+    n[3].own_address = 103;
     
-    n->number_of_connections = 3;
-    n->connections = malloc(sizeof(struct Connection) * 3);
+    n[3].number_of_connections = 3;
+    n[3].connections = malloc(sizeof(struct Connection) * 3);
     
-    n->connections->destination = 1;
-    n->connections->weight = 6;
-    n->connections += 1;
+    n[3].connections[0].destination = 1;
+    n[3].connections[0].weight = 6;
+
     
-    n->connections->destination = 19;
-    n->connections->weight = 1;
-    n->connections += 1;
+    n[3].connections[1].destination = 19;
+    n[3].connections[1].weight = 1;
 
+    n[3].connections[2].destination = 107;
+    n[3].connections[2].weight = 4;
 
-    n->connections->destination = 107;
-    n->connections->weight = 4;
-
-    n += 1;
 
     // New Node
-    n->own_address = 13;
-    n->number_of_connections = 3;
+    n[4].own_address = 13;
+    n[4].number_of_connections = 3;
     
-    n->connections = malloc(sizeof(struct Connection) * 3);
+    n[4].connections = malloc(sizeof(struct Connection) * 3);
     
-    n->connections->destination = 11;
-    n->connections->weight = 7;
-    n->connections += 1;
-
-    n->connections->destination = 101;
-    n->connections->weight = 4;
-    n->connections += 1;
+    n[4].connections[0].destination = 11;
+    n[4].connections[0].weight = 7;
 
 
-    n->connections->destination = 17;
-    n->connections->weight = 3;
+    n[4].connections[1].destination = 101;
+    n[4].connections[1].weight = 4;
 
 
 
-    n += 1;
+    n[4].connections[2].destination = 17;
+    n[4].connections[2].weight = 3;
+
 
     // New Node
-    n->own_address = 101;
+    n[5].own_address = 101;
 
-    n->number_of_connections = 3;
+    n[5].number_of_connections = 3;
     
-    n->connections = malloc(sizeof(struct Connection) * 3);
+    n[5].connections = malloc(sizeof(struct Connection) * 3);
     
-    n->connections->destination = 13;
-    n->connections->weight = 4;
-    n->connections += 1;
+    n[5].connections[0].destination = 13;
+    n[5].connections[0].weight = 4;
 
+    n[5].connections[1].destination = 19;
+    n[5].connections[1].weight = 2;
 
-    n->connections->destination = 19;
-    n->connections->weight = 2;
-    n->connections += 1;
     
-    n->connections->destination = 107;
-    n->connections->weight = 2;
+    n[5].connections[2].destination = 107;
+    n[5].connections[2].weight = 2;
   
-    n += 1;
+
 
     // New Node
-    n->own_address = 17;
+    n[6].own_address = 17;
 
-    n->number_of_connections = 2;
-    n->connections = malloc(sizeof(struct Connection) * 2);
+    n[6].number_of_connections = 2;
     
-    n->connections->destination = 13;
-    n->connections->weight = 3;
-    n->connections += 1;
+    n[6].connections = malloc(sizeof(struct Connection) * 2);
+    
+    n[6].connections[0].destination = 13;
+    n[6].connections[0].weight = 3;
 
-    n->connections->destination = 107;
-    n->connections->weight = 2;
+    n[6].connections[1].destination = 107;
+    n[6].connections[1].weight = 2;
 
-    n += 1;
+
 
     // New node
-    n->own_address = 107;
-    n->number_of_connections = 3;
-    n->connections = malloc(sizeof(struct Connection) * 3);
-
-    n->connections->destination = 17;
-    n->connections->weight = 2;
-    n->connections += 1;
+    n[7].own_address = 107;
+    n[7].number_of_connections = 3;
     
+    n[7].connections = malloc(sizeof(struct Connection) * 3);
 
-    n->connections->destination = 101;
-    n->connections->weight = 2;
-    n->connections += 1;
-    
-
-    n->connections->destination = 103;
-    n->connections->weight = 4;
+    n[7].connections[0].destination = 17;
+    n[7].connections[0].weight = 2;
+  
+    n[7].connections[1].destination = 101;
+    n[7].connections[1].weight = 2;
+   
+    n[7].connections[2].destination = 103;
+    n[7].connections[2].weight = 4;
     
     
 
