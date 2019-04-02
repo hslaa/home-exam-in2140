ad4465eb1e79a458e0d4917c575892c18f2f11ea
Henrik Slaaen <henrik@slaaen.com>
2019-04-02 18:39:43 +0200

    Continuing work on SSSP helper methods

--- util/sssp.c
+++ util/sssp.c
@@ -1,20 +1,39 @@
 /*
  *
- *  Code for the single source shortest path (SSSP) algorithmi and helper methods
+ *  Code for the single source shortest path (SSSP) algorithm and helper methods
  *
  *  Implementation of Dijkstra.
  *
  *
  */
-include 
+#include <string.h> 
+#include <math.h>
 
-struct Node extract_min(struct Node n*) {
+#include "../core/types/networktypes.h"
+#include "../util/helpers.h"
+
+struct Node extract_min(struct Node *n, int size_of_array) {
+    double min = INFINITY; // math.h
+    int min_index;
+    int i;
+    struct Node tmp_node;
     // Find the node with the smallest distance
-    //
+    while (i < size_of_array) {
+        if (n[i].distance < min) { /* This might fail because nodes are initialized to INF (Dijkstra) */
+            min_index = i;
+            min = n[i].distance;
+        }
+    }
+    tmp_node = n[min_index];
+
+
 
     // Memmove the last item of the list to them index we just
     // retrieved the node from
-    
+    memcpy(&n[min_index], &n[size_of_array], sizeof(struct Node));
+
+
+
     // Free the allocated memory for the smallest node  
 
 
@@ -23,7 +42,7 @@ struct Node extract_min(struct Node n*) {
 }
 
 
-void sssp(struct Node n*, struct Node source) {
+void sssp(struct Node *n, struct Node source) {
 
     //  Initialize a priority queue Q.
     //  Q sorts based on node.distance
