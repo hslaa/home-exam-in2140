d2fbf2a3666e305f1d72ee41c58685a4b06ae615
Henrik Slaaen <henrik@slaaen.com>
2019-04-02 14:12:27 +0200

    Just testing structs

--- core/node.c
+++ core/node.c
@@ -5,9 +5,18 @@
  *
  *
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "../util/logger.h"
+#include "types/networktypes.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    
+    struct Node this;
+    this.port = atoi(argv[1]);
+    this.own_address = atoi(argv[2]);
+    this.connections = malloc(sizeof(struct Connection)*(argc-3));
     logger("DEBUG", "Node starting");
+    free(this.connections);
 }
