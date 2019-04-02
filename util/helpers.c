31921f3c2ad1df7e9d591050aba1c89689e5e63c
Henrik Slaaen <henrik@slaaen.com>
2019-04-02 18:38:16 +0200

    Added helpers to free memory

--- util/helpers.c
+++ util/helpers.c
@@ -13,6 +13,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../core/types/networktypes.h"
 
@@ -31,3 +32,37 @@ int get_index_of_node(int own_address, int size_of_array, struct Node *node_arra
 
     return -1;
 }
+
+struct Node* get_pointer_to_node(int own_address, int size_of_array, struct Node *node_array) {
+
+    int i = 0;
+    
+    printf("trying to find node %d in a array of size %d\n", own_address, size_of_array);
+
+    while(i < size_of_array) {
+        if (node_array[i].own_address == own_address) {
+            return &node_array[i]; 
+        }
+        i += 1;
+    }
+
+    return NULL;
+}
+
+
+void free_connections(struct Node n) {
+    printf("Deleting connections on node %d\n", n.own_address);
+    free(n.connections);
+
+}
+
+void free_nodes(struct Node *n, int number_of_nodes) {
+    int i;
+
+    for (i = 0; i < number_of_nodes; i++) {
+        free_connections(n[i]);
+    }
+    free(n);
+}
+
+
--- util/helpers.h
+++ util/helpers.h
@@ -1,3 +1,5 @@
 #include "../core/types/networktypes.h"
 
 int get_index_of_node(int own_address, int size_of_array, struct Node *node_array);
+void free_connections(struct Node n);
+void free_nodes(struct Node *n, int number_of_nodes);
