bcb1ef7a545469650957161fa41cd9d33de6311f
Henrik Slaaen <henrik@slaaen.com>
2019-04-04 15:00:36 +0200

    Added declaration of get_pointer_to_node

--- util/helpers.h
+++ util/helpers.h
@@ -1,5 +1,6 @@
 #include "../core/types/networktypes.h"
 
 int get_index_of_node(int own_address, int size_of_array, struct Node *node_array);
+struct Node* get_pointer_to_node(int own_address, int size_of_array, struct Node *node_array);
 void free_connections(struct Node n);
 void free_nodes(struct Node *n, int number_of_nodes);
