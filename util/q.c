ab2d7c17cf163d55a925fcd9e62bcbf0e5825d77
Henrik Slaaen <henrik@slaaen.com>
2019-04-09 18:04:28 +0200

    Working version

--- util/q.c
+++ util/q.c
@@ -21,7 +21,9 @@ struct Node** initialize_queue(struct Node *n, int size_q) {
     q = malloc(sizeof(struct Node*) * size_of_q);
     
     for (i = 0; i < size_of_q; i++) {
+        printf("Inserting node %d into PriorityQueue with distance\n", n[i].own_address);
         q[i] = &n[i];
+        printf("(%d)\n", q[i]->own_address);
     }
 
 
@@ -29,34 +31,88 @@ struct Node** initialize_queue(struct Node *n, int size_q) {
 
 }
 
+int is_empty() {
+    return size_of_q;
+}
+/*
 struct Node* extract_min(struct Node **q) {
     int i;
-    double min_distance = INFINITY;
-   
+    int min_distance = 1000;
+    int min_index = 0; 
     
     struct Node *ret_node;
     
     if (size_of_q == 0) {
+        
         return NULL;
     }
 
     i = 0;
     while (i < size_of_q) {
         
-        if (q[i] == NULL) {
+        if (q[i] == NULL) { 
+            printf("q[%d] == NULL\n", i);
+            i += 1;
             continue;
         }
-
+        printf("Evaluating extract of node %d with distance %d\n", q[i]->own_address, q[i]->distance); 
         if (q[i]->distance < min_distance) {
             ret_node = q[i];
             min_distance = ret_node->distance;
+            min_index = i;
         }
 
         i += 1;
     
     }
+    q[min_index] = NULL;
     size_of_q -= 1;
+    if (ret_node != NULL) {
+        printf("=== Extracting node %d on index %d from PrioQueue\n", ret_node->own_address, min_index);
+    }
+    
+    printf("Finished loop #%d\n", i);
     return ret_node;    
+}
+*/
+struct Node* extract_min(struct Node **q) {
+    int i;
+    int min_distance = 100000;
+    int min_index = 0;
+
+    printf("START extract_min()\n"); 
+    struct Node *ret_node = NULL;
+
+    if(size_of_q == 0) {
+        printf("size_of_q == 0: returning NULL\n");
+        return NULL;
+    }
+
+    i = 0;
+    while(i < size_of_q) {
+        printf("\n\tchecking element %d\n", i);
+        if (q[i] == NULL) {
+            printf("\t\t element %d was NULL\n", i);
+            i += 1;
+            continue;
+        }
+
+        if (q[i]->distance < min_distance) {
+            printf("\t\t element %d replaces %d as the smallest element\n", i, min_index);
+            ret_node = q[i];
+            min_distance = ret_node->distance;
+            min_index = i;
+        }
+        i += 1; 
+    }
+    q[min_index] = NULL;
+    
+     
+
+    printf("END extract_min()\n"); 
+    return ret_node;    
+        
+
 }
 
 
--- util/q.h
+++ util/q.h
@@ -7,3 +7,4 @@
 
 struct Node** initialize_queue(struct Node *n, int size_q);
 struct Node* extract_min(struct Node **q);
+int is_empty();
