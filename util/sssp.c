ca2db7cb5675ee92e74d2719d3f9b5ab4745be15
Henrik Slaaen <henrik@slaaen.com>
2019-04-09 18:06:10 +0200

    Working sssp. Fixed NULL-check on extract_min

--- util/sssp.c
+++ util/sssp.c
@@ -33,14 +33,14 @@ void sssp(struct Node *n, struct Node source, int size) {
     //      n[i].distance = INF;
     //      n[i].previous = NULL;
     for (i = 0; i < size; i++) {
-        n[i].distance = INFINITY;
+        n[i].distance = 10000;
         n[i].previous = NULL;
     }
     
     
     
     //  source.distance = 0;
-    source.distance = 0;
+    n[0].distance = 0;
 
     // Does this remove the unused parameter warning?
     if (source.distance == 0) {
@@ -48,21 +48,28 @@ void sssp(struct Node *n, struct Node source, int size) {
     }
     
     //  while Q is not empty:
-    u = extract_min(q); 
+
     j = 0;
+    u = extract_min(q);
     while(u != NULL) {     
+        printf("=== Treating node %d\n", u->own_address);
         //for each connection in u.connections
         while (j < u->number_of_connections) {
             c = u->connections[j];
             alt = u->distance + c.weight;
+            printf("%d < %d", alt, c.destination->distance);
             if (alt < c.destination->distance) {
+                printf(" (TRUE)\n");
+                printf("Setting distance to: %d\n", alt);
                 c.destination->distance = alt;
                 c.destination->previous = u; 
+            } else {
+                printf(" (FALSE)\n");
             }
             j += 1;
         }
         j = 0;
-        u = extract_min(q);
+        u = extract_min(q); 
     }
     //
     //  return results. 
