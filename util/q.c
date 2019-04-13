ce79afa5cdc5191e5af0af1366cb83e74eb5cb29
Henrik Slaaen <henrik@slaaen.com>
2019-04-13 14:39:09 +0200

    Removed commented out code, which is replaced by code that actually works

--- util/q.c
+++ util/q.c
@@ -34,47 +34,7 @@ struct Node** initialize_queue(struct Node *n, int size_q) {
 int is_empty() {
     return size_of_q;
 }
-/*
-struct Node* extract_min(struct Node **q) {
-    int i;
-    int min_distance = 1000;
-    int min_index = 0; 
-    
-    struct Node *ret_node;
-    
-    if (size_of_q == 0) {
-        
-        return NULL;
-    }
 
-    i = 0;
-    while (i < size_of_q) {
-        
-        if (q[i] == NULL) { 
-            printf("q[%d] == NULL\n", i);
-            i += 1;
-            continue;
-        }
-        printf("Evaluating extract of node %d with distance %d\n", q[i]->own_address, q[i]->distance); 
-        if (q[i]->distance < min_distance) {
-            ret_node = q[i];
-            min_distance = ret_node->distance;
-            min_index = i;
-        }
-
-        i += 1;
-    
-    }
-    q[min_index] = NULL;
-    size_of_q -= 1;
-    if (ret_node != NULL) {
-        printf("=== Extracting node %d on index %d from PrioQueue\n", ret_node->own_address, min_index);
-    }
-    
-    printf("Finished loop #%d\n", i);
-    return ret_node;    
-}
-*/
 struct Node* extract_min(struct Node **q) {
     int i;
     int min_distance = 100000;
@@ -124,30 +84,6 @@ int free_queue(struct Node **q) {
     }
     free(q);
     printf("Finished freeing %d queue elements", i);
+
+    return 0;
 }
-/*
-int main() {
-
-    struct Node *testnodes = malloc(sizeof(struct Node) * 3);
-
-    testnodes[0].own_address = 100;
-    testnodes[0].distance = 60;
-
-    testnodes[1].own_address = 200;
-    testnodes[1].distance = 100;
-
-    testnodes[2].own_address = 123;
-    testnodes[2].distance = 0;
-    
-    struct Node **q = initialize_queue(testnodes, 3);
-
-    struct Node *smallestNode = extract_min(q);
-    
-    
-    printf("Address of smallest node: %d\n", smallestNode->own_address);
-
-    free(testnodes); 
-    free(q);
-}
-
-*/
