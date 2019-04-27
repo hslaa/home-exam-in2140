d69d300a9108dfa52d75528eccdd25510356a22c
Henrik Slaaen <henrik@slaaen.com>
2019-04-27 22:04:16 +0200

    adds code from transmission.c (now deleted)

--- core/protocol.c
+++ core/protocol.c
@@ -2,9 +2,28 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #include "types/networktypes.h"
 #include "../util/helpers.h"
+
+
+
+
+
+
+
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+
+
+#include "../util/print_lib.h"
+
+
+
+
+
+
 int parse_hops(unsigned char* data, int size_of_rt, struct Node* n);
 struct Connection* parse_connections(unsigned char* data, int number_of_connections); 
 int serialize_node(unsigned char* packet_buf, struct Node* n);
@@ -306,3 +325,79 @@ struct Connection* parse_connections(unsigned char* data, int number_of_connecti
     return connections; 
     
 }
+
+int deserialize_packet(struct packet* p, unsigned char *buf); 
+ 
+
+struct packet* create_packet(uint16_t destination_address, uint16_t source_address, unsigned char* message, int message_length) {
+    struct packet* p;
+    uint16_t packet_length = sizeof(uint16_t)*3 + message_length;
+    
+    p = malloc(sizeof(struct packet));
+    
+    p->packet_length = htons(packet_length);
+    p->destination_address = htons(destination_address);
+    p->source_address = htons(source_address);  
+  
+    memcpy(&(p->message), message, message_length+1);
+
+    return p;
+}
+
+unsigned char* serialize_packet(struct packet* p, int message_length) {
+    unsigned char *buf;
+    uint16_t  length;
+
+    //buf = malloc(sizeof(struct packet));
+    
+    length = (sizeof(uint16_t) * 3) + message_length + 1;
+    buf = malloc(length);
+    
+    length = htons(length);
+
+    printf("Mallocing %d bytes for serialization_buffer\n", length);
+    memcpy(&(buf[0]), &(length), sizeof(uint16_t));    
+    memcpy(&(buf[2]), &(p->destination_address), sizeof(uint16_t));    
+    memcpy(&(buf[4]), &(p->source_address), sizeof(uint16_t));     
+    memcpy(&(buf[6]), p->message, message_length); // + 1 
+    
+     
+    buf[ntohs(length)-1] = '\0';
+    
+    return buf;
+}
+
+int deserialize_packet(struct packet* p, unsigned char *buf) {
+ 
+    uint16_t message_length;
+
+
+
+    memcpy(&p->packet_length, &(buf[0]), sizeof(uint16_t)); 
+    memcpy(&p->destination_address, &(buf[2]), sizeof(uint16_t));
+    memcpy(&p->source_address, &(buf[4]), sizeof(uint16_t));
+    
+    p->packet_length = ntohs(p->packet_length); 
+    p->destination_address = ntohs(p->destination_address); 
+    p->source_address = ntohs(p->source_address); 
+
+
+    message_length = p->packet_length;
+    printf("deserialize_packet: trying to deserialize message of packet with length %d\n", p->packet_length);   
+    
+    printf("trying to memcpy(p->message, &(buf[6]), %d) on buffer of size 2048(?)\n", message_length); 
+    memcpy(p->message, &(buf[6]), message_length);
+    
+    return 0; 
+        
+
+}
+
+
+
+
+
+
+
+
+
