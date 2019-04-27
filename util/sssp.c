df8f4d5631a4b3d267850bdd26fd4377a3d51719
Henrik Slaaen <henrik@slaaen.com>
2019-04-27 22:05:37 +0200

    add q function declarations

--- util/sssp.c
+++ util/sssp.c
@@ -13,10 +13,15 @@
 
 #include "../core/types/networktypes.h"
 #include "../util/helpers.h"
-#include "q.h"
+
 
 void sssp(struct Node *n, int size);
 void generate_routing_tables(struct Node* n, int size);
+
+int free_queue(struct Node **q);
+struct Node** initialize_queue(struct Node *n, int size_q);
+struct Node* extract_min(struct Node **q);
+int is_empty();
 
 
 void sssp(struct Node *n, int size) {
